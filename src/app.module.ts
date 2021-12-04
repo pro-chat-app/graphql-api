c69f83b6dc117daa3569ed49f3565433742b0bdd
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2021-12-04 09:21:05 +0100

    :sparkles: Added hello world module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      installSubscriptionHandlers: true,
+    }),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
