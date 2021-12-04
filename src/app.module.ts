f2ac7cf08878d73b8269478ff44bb6671a7d53b5
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2021-12-04 09:20:47 +0100

    :fire: Removed unused app module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
 })
 export class AppModule {}
