53f212e5ccd11ed860b93f0f77f701ae4762cf99
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2021-12-04 10:41:09 +0100

    :sparkles: Configured database connexion to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import configuration from './config/configuration';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
       installSubscriptionHandlers: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('database'),
+      inject: [ConfigService],
     }),
     HelloWorldModule,
   ],
---
+++ src/config/configuration.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: process.env.NODE_ENV !== 'production',
+  },
+});
